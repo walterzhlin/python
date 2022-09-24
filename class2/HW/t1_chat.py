0aeb0c2adec3a8753e04f1eecd4df2073f141501
walterzhlin <walterzhlin@gmail.com>
2022-09-24 16:13:31 +0800

    class2

---
+++ class2/HW/t1_chat.py
@@ -0,0 +1,11 @@
+"""
+Topic:請使用input and print打造對話機器人
+
+e.g.
+old = input("How old are you?")
+print("I am " + old)
+
+1.Show:How old are you?
+2.input:12
+3.Output:I am 12
+"""
---
+++ class2/class/class.py
@@ -0,0 +1,26 @@
+'''
+max(找出最大值)
+len(分辨長度(字串))
+type(分辨資料)
+str(轉成文字)
+float(轉成浮點數)
+int(整數)
+bool(轉成布林值)
+print(顯示)
+input(輸入資料)
+'''
+'''
+print('可以把輸入的東西存起來')
+a=input('123456789=987654321')
+print('輸入的內容是')
+print(a)
+print('透過input輸入的資料型態為')
+print(type(a))
+'''
+# a=input('請輸入年齡:')
+# print('你的年齡是'+a+'歲')
+
+r = float(input('請輸入圓半徑:'))
+pi = 3.14
+area = pi * r**2
+print(area)
