a16e20b9eac43f4f3607e7b52213034f68860134
walterzhlin <walterzhlin@gmail.com>
2022-10-15 15:46:38 +0800

    提示文字

---
+++ class4/class/class.py
@@ -0,0 +1,57 @@
+'''
+not True
+not False
+not 1==1
+not not False
+
+True and True
+True and False
+False and True
+False and False
+
+True or True
+True or False
+False or True
+False or False
+
+'''
+'''
+y = input('芝麻開門')
+if y == '5678':
+    print('芝麻開門')
+elif y == '9487':
+    print('薛仔是笨蛋')
+else:
+    print('薛仔是笨蛋')
+'''
+x = int(input('分數'))
+'''
+if x >= 90:
+    print('A')
+if x >= 80 and x <= 89:
+    print('B')
+if x >= 70 and x <= 79:
+    print('C')
+if x >= 60 and x <= 69:
+    print('D')
+if x <= 59:
+    print('E')
+
+if x >= 90:
+    print('A')
+elif x >= 80:
+    print('B')
+elif x >= 70:
+    print('C')
+elif x >= 60:
+    print('D')
+elif x <= 59:
+    print('E')
+'''
+'''
+x=int(input('請輸入整數:'))
+if x%2==0:
+    print("是偶數")
+else:
+    print("是奇數")
+'''
