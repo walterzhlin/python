c894da6ffbe9a52229951e52fe5d9a2ad06753ca
walterzhlin <walterzhlin@gmail.com>
2022-11-19 13:58:35 +0800

    功課

--- class7/HW/hw.py
+++ class7/HW/hw.py
@@ -18,3 +18,36 @@ EX:
 請輸入23~25的整數:24
 恭喜猜中!
 '''
+import random as r
+
+a = 100
+b = 0
+x = r.randrange(0, 101)
+while True:
+    ans = int(input(f"請輸入{b}~{a}的整數:"))
+    if ans > x:
+        print('在小一點')
+        if b < ans < a:
+            a = ans
+    elif ans < x:
+        print('在大一點')
+        if b < ans < a:
+            b = ans
+    else:
+        print('~恭喜猜中了!~')
+        break
+git add .
+git commit -m "提示文字"
+git push
+
+
+
+
+
+
+
+
+
+
+
+
