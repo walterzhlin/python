b1ea9127e01bfe54dd8f4694e56b08463b829ee3
walterzhlin <walterzhlin@gmail.com>
2022-12-17 15:34:18 +0800

    筆記

---
+++ class12/class/class.py
@@ -0,0 +1,28 @@
+# print('I %d savage' % 94)
+# print('I %3d savage' % 94)
+# print('I %03d savage' % 94)
+# print('I {0:d} savage'.format (94))
+# print('I {0:3d} savage'.format (94))
+# print('I {0:03d} savage'.format (94))
+# print('I %f savage' % 94)
+# print('I %.2f savage' % 94)
+# print('I {0:f} savage'.format (94))
+# print('I {0:.2f} savage'.format (94))
+# print('I %s savage' % '94')
+# print('I {0:s} savage'.format (94))
+# a = int(input('輸入您要的數值'))
+
+# print('%d' % a)
+# print('%5d'%a)
+# print('%05d'%a)
+# a = float(input('輸入您要的數值'))
+
+# print('%f' % a)
+# print('%.2f' % a)
+# a = 5
+# b = 5
+# ans = eval('a+b')
+# print(ans)
+import datetime
+
+print(datetime.date.today())
